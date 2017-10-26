33857e6f60213935e8c0534fcc16ab36aa99852e
Naveen Bansal <naveen18jan93@gmail.com>
2017-10-26 02:26:59 -0400

    added and trained machine learning models. Added code to generate analytics

--- userservice/user.py
+++ userservice/user.py
@@ -1,18 +1,31 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, request, json
+
+from flask import Flask, jsonify, abort, request, json, make_response
 from mongoConnection import getClient
+import uuid
+import datetime
+import httperrors
+from session import createSession
+from bson.objectid import ObjectId
 import hashlib
 
 app = Flask(__name__)
 
-@app.route('/userservice/api/v1.0/user/<string:userid>', methods=['GET'])
-def getUser(userid):
-	db = getClient()
-	cursor = db['hrservice']
-	docs = cursor.users.find({"sessionid":sessionid})
-	for document in docs:
-		return jsonify(session_valid = True)
-	return jsonify(session_valid = False)
+@app.route('/userservice/api/v1.0/user', methods=['GET'])
+def getUser():
+	sessionid = request.headers.get("sessionid")
+	userid = request.headers.get("userid")
+	response = {}
+	if not isValidSessionId(sessionid, userid):
+		response["message"] = "Your session has expired, Please login again"
+		response["code"] = httperrors.UNAUTHORIZED_ERROR
+		return jsonify(success = False, error = response)
+	doc = getUserDetails(userid)
+	response["userid"] = str(doc["_id"])
+	response["sessionid"] = sessionid
+	response["firstname"] = doc["firstname"]
+	response["lastname"] = doc["lastname"]
+	return jsonify(success = True, data = response)
 
 @app.route('/userservice/api/v1.0/user', methods=['PUT'])
 def createUser():
@@ -20,40 +33,66 @@ def createUser():
 	bodyparam = request.data
 	bodyparam = json.loads(bodyparam)
 	isValid = validateParams(bodyparam)
+	response = {}
 	if not isValid:
-		return jsonify(message = "Invalid body param")
-	print(bodyparam["password"])
+		response["message"] = "Invalid body param"
+		response["code"] = httperrors.BAD_REQUEST_ERROR
+		return jsonify(success = False, error = response["error"])
 	hashedPassword  = hashlib.sha224(bodyparam["password"].encode('utf-8')).hexdigest()
 	cursor = db['hrservice']
-	doc  = cursor.users.find({"email":bodyparam["email"]})
+	doc  = cursor.users.find_one({"email":bodyparam["email"]})
 	if doc is not None:
-		return jsonify(success = True, message = "user with this email already exists")
+		response["message"] = "user with this email already exists"
+		response["code"] = httperrors.BAD_REQUEST_ERROR
+		return jsonify(success = False, error = response)
 	result = cursor.users.insert_one({"firstname":bodyparam["firstname"], "lastname":bodyparam["lastname"], "email":bodyparam["email"], "password":bodyparam["password"]})
 	if result is not None:
 		return jsonify(success = True)
 	return jsonify(success = False)
 
+@app.route('/userservice/api/v1.0/user/login', methods=['PUT'])
 def login():
 	db = getClient()
+	cursor = db['hrservice']
 	bodyparam = request.data
 	bodyparam = json.loads(bodyparam)
 	hashedPassword  = hashlib.sha224(bodyparam["password"].encode('utf-8')).hexdigest()
-	docs = db.users.find({"userid":bodyparam["userid"], "password":bodyparam["password"]})
+	docs = cursor.users.find_one({"email":bodyparam["email"], "password":bodyparam["password"]})
 	if docs is None:
 		return jsonify(user_valid = False, message = "Invalid email or passord")
 	else:
-	 return jsonify(user_valid = True)
+		sessionid = str(uuid.uuid4())
+		expire_date = datetime.datetime.now()
+		expire_date = expire_date + datetime.timedelta(days=10)
+		response = make_response(jsonify(success = True, sessionid = sessionid))
+		response.set_cookie("sessionid", sessionid, expires=expire_date)
+		response.set_cookie("userid", str(docs["_id"]), expires=expire_date)
+		return response
 
 def deleteUser(userid):
 	db = getClient()
 	cursor = db['hrservice']
-	result = cursor.users.delete_one({"userid":userid})
+	result = cursor.users.delete_one({"userid":ObjectId(userid)})
 
 def validateParams(body):
 	for key, value in body.items():
 		if len(value.strip()) < 3:
 			return False
 	return True
+
+def isValidSessionId(sessionid, userid):
+	db = getClient()
+	cursor = db['hrservice']
+	docs = cursor.session.find_one({"sessionid":sessionid, "userid":userid})
+	if docs is None:
+		return False;
+	return True
+
+def getUserDetails(userid):
+	db = getClient()
+	cursor = db['hrservice']
+	docs = cursor.users.find_one({"_id":ObjectId(userid)})
+	return docs
 
 if __name__ == '__main__':
 	# getSession("jjfdjdhfgjfd763276")
