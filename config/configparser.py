2a99bef0c60d60b44feb569fb699a4337d46e0fd
Naveen Bansal <naveen18jan93@gmail.com>
2017-11-19 20:37:59 -0500

    using mongodb instance on cloud

--- config/configparser.py
+++ config/configparser.py
@@ -9,8 +9,16 @@ class Config:
 		with open(config_file_path) as config_file:
 			config = json.load(config_file)
 		self.project_directory = os.environ['HR_SERVICE_ROOT_DIR']
-		self.mongo_path = os.environ['MONGO_PATH']
+		# self.mongo_path = os.environ['MONGO_PATH']
+		self.mongo_password = os.environ['MONGO_PASS']
+		self.mongo_username = os.environ['MONGO_USERNAME']
 		self.hash_salt = os.environ['HASH_SALT']
+		self.mongo_shard = ["", "", ""]
+		self.mongo_shard[0] = config["mongo-shard-0"]
+		self.mongo_shard[1] = config["mongo-shard-1"]
+		self.mongo_shard[2] = config["mongo-shard-2"]
+		self.mongo_replica = config["mongo-replica"]
+
 
 def getConfig():
 	global config
@@ -21,5 +29,5 @@ def getConfig():
 if __name__ == "__main__":
 	conf = getConfig()
 	print(conf.project_directory);
-	print(conf.mongo_path);
+	# print(conf.mongo_path);
 	print(conf.hash_salt)
